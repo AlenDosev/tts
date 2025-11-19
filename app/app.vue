aa44ff26d672de8f742ef3bdb3e4d0e135ae8dd3
Alen Dosev <alen@apptitude.nl>
2025-11-19 20:26:08 +0100

    feat(): logo

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,19 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <nuxt-img class="test" src="https://scontent-ams2-1.cdninstagram.com/v/t51.82787-15/521121077_18072583147978092_4334997575411950480_n.jpg?stp=dst-jpg_e35_tt6&_nc_cat=106&ig_cache_key=MzY4MTY3NTQ3OTI5NDg2NTkzMg%3D%3D.3-ccb7-5&ccb=7-5&_nc_sid=58cdad&efg=eyJ2ZW5jb2RlX3RhZyI6InhwaWRzLjEzNTB4MTY4OC5zZHIuQzMifQ%3D%3D&_nc_ohc=o3pO-4FF6UQQ7kNvwFUbvbj&_nc_oc=Adm0Bk1pPpsduQOIWZT0Zi4HzqoDIX1dWhgLNaiyp5D-Kcy833Ltp0ESkgmGB2PXJBM&_nc_ad=z-m&_nc_cid=0&_nc_zt=23&_nc_ht=scontent-ams2-1.cdninstagram.com&_nc_gid=fO-S6lRsg--5gtn0VI2e2A&oh=00_AfjPY_Q_2FDha2dbR4BUFZSnfxwDYuYP8k2rakrFSScZ1Q&oe=6923FF76" />
   </div>
 </template>
+<style lang="css">
+.test {
+  max-height: 100dvh;
+  height: 100dvh;
+}
+
+body {
+  background-color: black;
+  display: flex;
+  justify-self: center;
+  margin: 0;
+  overflow: hidden;
+}
+</style>
